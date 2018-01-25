fb9c6b888f3411b797377d268ce6b71afc24dfa7
jonenxs <570141370@qq.com>
2018-01-25 16:47:04 +0800

    Service层单元测试

---
+++ src/test/java/org/seckill/service/SeckillServiceTest.java
@@ -0,0 +1,63 @@
+package org.seckill.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExecution;
+import org.seckill.entity.Seckill;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({
+        "classpath:spring/spring-dao.xml",
+        "classpath:spring/spring-service.xml"})
+public class SeckillServiceTest {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SeckillService seckillService;
+
+    @Test
+    public void getSeckillList() {
+        List<Seckill> list = seckillService.getSeckillList();
+        logger.info("list={}",list);
+    }
+
+    @Test
+    public void getById() {
+        long id = 1000L;
+        Seckill seckill = seckillService.getById(id);
+        logger.info("seckill={}",seckill);
+    }
+
+    @Test
+    public void testSeckillLogic() throws Exception{
+        long id = 1000;
+        long phone = 13812345789L;
+        Exposer exposer = seckillService.exportSeckillUrl(id);
+        if (exposer.getExposed()) {
+            logger.info("exposer={}",exposer);
+            try {
+                SeckillExecution execution = seckillService.executeSeckill(id, phone, exposer.getMd5());
+                logger.info("result={}", execution);
+            } catch (RepeatKillException e) {
+                logger.error(e.getMessage());
+            } catch (SeckillCloseException s) {
+                logger.error(s.getMessage());
+            }
+        }else{
+            logger.warn("expoer={}",exposer);
+        }
+    }
+}
