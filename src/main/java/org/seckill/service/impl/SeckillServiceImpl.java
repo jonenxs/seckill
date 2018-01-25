f88ce0ce4e40e523f54a7427a50c79715166fe80
jonenxs <570141370@qq.com>
2018-01-25 15:36:36 +0800

    spring声明式事务

--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -15,6 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
 import java.util.Date;
@@ -68,6 +69,13 @@ public class SeckillServiceImpl implements SeckillService {
         return new Exposer(true,md5,seckillId);
     }
 
+    /**
+     *使用注解控制事物的优点
+     * 1：开发团队达成一致约定，明确标注事务的编程风格
+     * 2：保证事务方法的执行时间尽可能短，不要穿插其它网络操作，RPC/HTTP操作或者剥离到事务方法外部
+     * 3：不是所有的方法都需要事务，如只有一条修改，只读操作不需要事务.
+     */
+    @Transactional
     public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5) throws SeckillException, RepeatKillException, SeckillCloseException {
         if (md5 == null || !md5.equals(getMD5(seckillId))) {
             throw new SeckillException("seckill data rewrite");
