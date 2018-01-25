40929f2a63df154a677b56f9c268b2b4142a6b3c
jonenxs <570141370@qq.com>
2018-01-25 15:18:19 +0800

    Service 与 Spring集成

---
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -0,0 +1,115 @@
+package org.seckill.service.impl;
+
+import org.seckill.dao.SeckillDao;
+import org.seckill.dao.SuccessKilledDao;
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExecution;
+import org.seckill.entity.Seckill;
+import org.seckill.entity.SuccessKilled;
+import org.seckill.enums.SeckillStateEnum;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.seckill.exception.SeckillException;
+import org.seckill.service.SeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author 57014
+ */
+@Service
+public class SeckillServiceImpl implements SeckillService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SeckillDao seckillDao;
+
+    @Autowired
+    private SuccessKilledDao successKilledDao;
+
+    /**
+     * md5加密盐值字符串，用于混淆md5
+     */
+    private final String slat = "sfdfdfhgjfhgfsjhgjjkubab84853#$#2sdf";
+
+    public List<Seckill> getSeckillList() {
+        return seckillDao.queryAll(0, 4);
+    }
+
+    public Seckill getById(long seckillId) {
+        return seckillDao.queryById(seckillId);
+    }
+
+    public Exposer exportSeckillUrl(long seckillId) {
+        Seckill seckill = this.getById(seckillId);
+        if (seckill == null) {
+            //秒杀商品不存在
+            return new Exposer(false, seckillId);
+        }
+        Date startTime = seckill.getStartTime();
+        Date endTime = seckill.getEndTime();
+        //系统当前时间
+        Date nowTime = new Date();
+        if (nowTime.getTime() < startTime.getTime() ||
+                nowTime.getTime() > endTime.getTime()) {
+            //秒杀还未开启或者已结束
+            return new Exposer(false,seckillId,nowTime.getTime(),startTime.getTime(),endTime.getTime());
+        }
+        //转化特定字符串的过程，不可逆
+        String md5 = getMD5(seckillId);
+        return new Exposer(true,md5,seckillId);
+    }
+
+    public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5) throws SeckillException, RepeatKillException, SeckillCloseException {
+        if (md5 == null || !md5.equals(getMD5(seckillId))) {
+            throw new SeckillException("seckill data rewrite");
+        }
+        try {
+            //执行秒杀逻辑：减库存，记录购买行为
+            Date nowTime = new Date();
+            int updateCount = seckillDao.reduceNumber(seckillId, nowTime);
+            if (updateCount <= 0) {
+                //没有更新记录，秒杀失败
+                throw new SeckillCloseException("seckill is closed");
+            }else {
+                //减库存成功，记录购买行为
+                int insertCount = successKilledDao.insertSuccessKilled(seckillId, userPhone);
+                if (insertCount <= 0) {
+                    //重复秒杀
+                    throw new RepeatKillException("seckill repeated");
+                } else {
+                    //秒杀成功
+                    SuccessKilled successKilled = successKilledDao.queryByIdWithSeckill(seckillId, userPhone);
+                    return new SeckillExecution(seckillId, SeckillStateEnum.SUCCESS,successKilled);
+                }
+            }
+        }catch (SeckillCloseException seckillCloseException){
+          throw seckillCloseException;
+        }catch (RepeatKillException repeatKillException){
+            throw repeatKillException;
+        }catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            //所有编译期异常 转化为运行期异常
+            throw new SeckillException("seckill inner error:" + e.getMessage());
+        }
+    }
+
+    /**
+     * 获取md5
+     * @param seckillId
+     * @return
+     */
+    private String getMD5(long seckillId) {
+        String base = seckillId + "/" + slat;
+        String md5 = DigestUtils.md5DigestAsHex(base.getBytes());
+        return md5;
+    }
+}
